1cafe5386433f1d25b57727885395d14eb27cc9b
Thomas Beecher <Thomasbeecher2@gmail.com>
2023-02-05 15:17:27 -0500

    Testing nav functionality

--- Header.js
+++ Header.js
@@ -1,36 +1,17 @@
 class Header extends HTMLElement {
     connectedCallback() {
-     /* this.innerHTML = `           
-        <ul>
-        <li>
-          <a href="#">Home</a>
-        </li>
-        <li>
-          <a href="#" ">GitHub Projects</a>
-          <ul>
-            <li><a href="#">Pac-Man Exercise</a></li>
-            <li><a href="#">Eyes Exercise</a></li>
-            <li><a href="#">Real Time Bus Tracking</a></li>
-            <li><a href="https://github.com/Beechcreech" target="_blank">GitHub</a></li>
-          </ul>
-        </li>
-        <li>
-          <a href="bio.html">Bio</a>
-        </li>
-      </ul>      
-      `; */
        this.innerHTML = `   
        <header>        
        <ul class="nav nav-pills">
        <li class="nav-item">
-         <a class="nav-link" aria-current="page" href="#">Home</a>
+         <a class="nav-link" aria-current="page" href="index.html">Home</a>
        </li>
        <li class="nav-item dropdown">
          <a class="nav-link dropdown-toggle" data-bs-toggle="dropdown" href="#" role="button" aria-expanded="false">GitHub Projects</a>
          <ul class="dropdown-menu">
-           <li><a class="dropdown-item" href="#">Pac-Man Exercise</a></li>
-           <li><a class="dropdown-item" href="#">Eyes Exercise</a></li>
-           <li><a class="dropdown-item" href="#">Real Time Bus Tracking</a></li>
+           <li><a class="dropdown-item" href="projects/Pacman.html">Pac-Man Exercise</a></li>
+           <li><a class="dropdown-item" href="projects/Eyes.html">Eyes Exercise</a></li>
+           <li><a class="dropdown-item" href="projects/Bus.html">Real Time Bus Tracking</a></li>
            <li><hr class="dropdown-divider"></li>
            <li><a class="dropdown-item" href="https://github.com/Beechcreech" target="_blank">GitHub</a></li>
          </ul>
